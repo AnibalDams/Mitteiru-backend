ff9521e07960e3bcb2d97f8e297e05c1e34a1e9d
Anibal Dams <75810272+AnibalDams@users.noreply.github.com>
2025-01-25 12:15:16 -0400

    Update db.ts

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -4,6 +4,7 @@ const uri:any = "mongodb+srv://dams:dams@cluster0.fvwbh.mongodb.net/?retryWrites
 const client = new MongoClient(uri, {
   
   serverApi: {
+    
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
@@ -13,14 +14,11 @@ const client = new MongoClient(uri, {
 });
 
 export default async function run() {
-  try {
+
 
     await client.connect();
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-
-    await client.close();
-  }
+  
 }
