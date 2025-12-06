16cfa662c96c416e43e81a35843091f0d2481701
Aníbal Dams <elnini120@gmail.com>
2025-12-06 13:59:30 -0400

    Update dbClient.ts

--- src/libs/dbClient.ts
+++ src/libs/dbClient.ts
@@ -1,5 +1,5 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
-  const uri:any = "mongodb://localhost";
+  const uri:any = process.env.DATABASE;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
